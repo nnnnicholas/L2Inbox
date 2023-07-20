1a2ecfa44bfb944355579ca3fd61496bbc004c7f
nicholas <nnnnicholas@users.noreply.github.com>
2023-07-20 15:26:01 -0400

    remove superfluous encrypted hash

--- src/Inbox.sol
+++ src/Inbox.sol
@@ -3,31 +3,26 @@ pragma solidity ^0.8.19;
 
 ///@author nnnnicholas
 contract Inbox {
-
     // Event emitted when a message is sent
     event MessageSent(
         address indexed sender,
         address indexed recipient,
-        bytes32 indexed encryptedMessageHash,
         bytes32 plaintextHash,
         bytes encryptedMessage,
         uint256 amount
     );
 
     // Event emitted when a message is revealed
-    event MessageRevealed(
-        address indexed revealer,
-        string plaintext
-    );
+    event MessageRevealed(address indexed revealer, string plaintext);
 
     // Function to send a message
-    function sendMessage(address recipient, bytes32 encryptedMessageHash, bytes calldata encryptedMessage, bytes32 plaintextHash) external payable {
+    function sendMessage(address recipient, bytes calldata encryptedMessage, bytes32 plaintextHash) external payable {
         // Forward any ether sent with the transaction to the recipient
         (bool success, ) = payable(recipient).call{value: msg.value}("");
         require(success, "Payment failed");
 
         // Emit the event
-        emit MessageSent(msg.sender, recipient, encryptedMessageHash, plaintextHash, encryptedMessage, msg.value);
+        emit MessageSent(msg.sender, recipient, plaintextHash, encryptedMessage, msg.value);
     }
 
     // Function to reveal a message
