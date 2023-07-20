1ff74eb55b96a44528cec6e1813d53dd86f0b0a6
nicholas <nnnnicholas@users.noreply.github.com>
2023-07-20 14:04:32 -0400

    first pass contract

---
+++ src/Inbox.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract Inbox {
+    // Author of the contract
+    address public author = msg.sender;
+
+    // Event emitted when a message is sent
+    event MessageSent(
+        address indexed sender,
+        address indexed recipient,
+        bytes32 indexed encryptedMessageHash,
+        bytes32 plaintextHash,
+        bytes encryptedMessage,
+        uint256 amount
+    );
+
+    // Event emitted when a message is revealed
+    event MessageRevealed(
+        address indexed revealer,
+        string plaintext
+    );
+
+    // Function to send a message
+    function sendMessage(address recipient, bytes32 encryptedMessageHash, bytes calldata encryptedMessage, bytes32 plaintextHash) external payable {
+        // Forward any ether sent with the transaction to the recipient
+        (bool success, ) = payable(recipient).call{value: msg.value}("");
+        require(success, "Payment failed");
+
+        // Emit the event
+        emit MessageSent(msg.sender, recipient, encryptedMessageHash, plaintextHash, encryptedMessage, msg.value);
+    }
+
+    // Function to reveal a message
+    function revealMessage(string calldata plaintext) external {
+        // Emit the event
+        emit MessageRevealed(msg.sender, plaintext);
+    }
+}
